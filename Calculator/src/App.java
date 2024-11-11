31b7c047bd7a5866bae54cbbefd6b7518c45a151
Santos Odigie <santosodigie104@gmail.com>
2024-11-11 08:52:12 +0000

    completed calculator logic

--- Calculator/src/App.java
+++ Calculator/src/App.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Hello, Gang!");
+        System.out.println("Hello, User!");
         System.out.println("Welcome to my simple Calculator App in Java");
 
         boolean exit = false;
@@ -18,7 +18,72 @@ public class App {
             System.out.println("5. Exit");
 
             String choice = myObj.nextLine();
+
+            switch(choice)
+            {
+                case "1":
+                performOperation("Addition");
+                break;
+                case "2":
+                performOperation("Subtraction");
+                break;
+                case "3":
+                performOperation("Multiplication");
+                break;
+                case "4":
+                performOperation("Division");
+                break;
+                case "5":
+                exit = true;
+                break;
+                default:
+                System.out.println("Please enter a valid option!");
+            }
         }
     }
-    
+    public static void performOperation(String operation){
+        System.out.println("You Chose " + operation);
+        double num1 = getValidNumber("Please enter the first number: ");
+        double num2 = getValidNumber("Please enter the second number: ");
+
+        double result = 0;
+
+        switch(operation)
+        {
+            case "Addition":
+            result = num1 + num2;
+            break;
+            case "Subtraction":
+            result = num1 - num2;
+            break;
+            case "Multiplication":
+            result = num1 * num2;
+            break;
+            case "Division":
+            result = num1 / num2;
+            break;
+
+        }
+
+    }
+
+    public static double getValidNumber(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        double number;
+        while(true){
+            System.out.println(prompt);
+            String input = scanner.nextLine();
+
+            try {
+                number = Double.parseDouble(input);
+                break; // exiting loop if parsing succeeds
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+
+        }
+
+
+        return number;
+    }
 }
